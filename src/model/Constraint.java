06a838261ad871cd4d6d774390b34d9fc603fea6
math.herbert <math.herbert@gmail.com>
2014-10-17 10:32:27 +0200

    add Constraint

---
+++ src/model/Constraint.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by math.herbert on 17/10/14.
+ */
+public class Constraint {
+    private List<ForbiddenRegion> forbiddenRegions;
+
+    public Constraint(){
+        this.forbiddenRegions =  new ArrayList<ForbiddenRegion>();
+    }
+
+    public void addForbiddenRegion(ForbiddenRegion forbiddenRegion){
+        this.forbiddenRegions.add(forbiddenRegion);
+    }
+
+    public List<ForbiddenRegion> getForbiddenRegions(){
+        return this.forbiddenRegions;
+    }
+
+}
